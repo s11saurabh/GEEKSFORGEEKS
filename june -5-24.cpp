b480150987aa6866a9745ebc5e4728fe56be5f2a
s11saurabh <saurabhsinghania111@gmail.com>
2024-06-05 21:23:06 +0530

    Create june -5-24.cpp

---
+++ june -5-24.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+
+ 
+
+  public:
+
+    int findSwapValues(int a[], int n, int b[], int m) {
+
+    int sumA = 0, sumB = 0;
+
+    for (int i = 0; i < n; ++i) sumA += a[i];
+
+    for (int i = 0; i < m; ++i) sumB += b[i];
+
+    
+
+    int diff = sumA - sumB;
+
+    if (diff % 2 != 0) return -1;
+
+ 
+
+    int target = diff / 2;
+
+    unordered_set<int> setA(a, a + n);
+
+    
+
+    for (int i = 0; i < m; ++i)
+
+        if (setA.count(b[i] + target)) return 1;
+
+ 
+
+    return -1;
+
+}
+
+ 
+
+ 
+
+};
